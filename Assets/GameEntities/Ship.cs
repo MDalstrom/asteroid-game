89e937a7cdc0c9d00313341d531c9a3b98c4fe3e
MDalstrom <MDalstrom@yandex.ru>
2021-09-14 13:03:07 +0700

    Initial commit

---
+++ Assets/GameEntities/Ship.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _rotatingSpeed;
+    [SerializeField] private float _acceleration;
+
+    private Vector3 _movingDirection;
+
+    public void Shoot()
+    {
+
+    }
+    public void Rotate(bool positive)
+    {
+        transform.Rotate(Vector3.one * _rotatingSpeed * (positive ? 1 : -1));
+    }
+    public void AddAcceleration(bool positive)
+    {
+        _movingDirection += transform.up * _acceleration * (positive ? 1 : -1);
+        _movingDirection = Vector3.ClampMagnitude(_movingDirection, _maxSpeed);
+    }
+
+    private void Update()
+    {
+        transform.position += _movingDirection;
+    }
+}
